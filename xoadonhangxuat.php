d59f549be357e459c2b7c7a97311d7c7337f5ae4
thiet003 <thietdong264@gmail.com>
2024-05-24 19:56:37 +0700

    add

--- xoadonhangxuat.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET['id'];
-require 'connect.php';
-$sql = "delete from donxuatchitiet where id_donxuat = '$id'";
-mysqli_query($connect,$sql);
-$sql = "delete from donxuat where id = '$id'";
-mysqli_query($connect,$sql);
-header('location:danhsachdonhangxuat.php');
-?>
